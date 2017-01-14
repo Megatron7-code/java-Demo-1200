97441204ae88d3225a43206c916ba79e508fbed5
xushuai <553782330@qq.com>
2017-01-14 17:36:59 +0800

    Triangle demo

---
+++ src/com/vm321/chapter1/Triangle.java
@@ -0,0 +1,14 @@
+package com.vm321.chapter1;
+
+/**
+ * Created by Megatron on 2017/1/14.
+ */
+public class Triangle {
+    public static void main(String[] args){
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  ******");
+        System.out.println(" ********");
+        System.out.println("**********");
+    }
+}
