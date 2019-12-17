adc8997a74fc2660bb58892b5b9c908b9fb26751
megatron <megatron@megatrondeiMac.local>
2019-12-17 19:16:03 +0800

    adjust

---
+++ src/com/vm323/d121/demo.java
@@ -0,0 +1,15 @@
+package com.vm323.d121;
+
+public class demo {
+    public static void main(String[] args) {
+        double res = (1 + 2.236)/2;
+        double res2 = 1 + 2 + 3 + 4.0;
+        boolean res3 = 4.1 >= 4;
+        String res4 = 1 + 2 + "3";
+
+        System.out.println("res: " + res);
+        System.out.println("res2: " + res2);
+        System.out.println("res3: " + res3);
+        System.out.println("res4: " + res4);
+    }
+}
